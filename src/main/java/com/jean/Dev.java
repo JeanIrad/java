78473d06029b137455daccdd5538453236a24e60
JeanIrad <jado.milton@gmail.com>
2024-11-30 06:25:29 +0200

    initial commit

---
+++ src/main/java/com/jean/Dev.java
@@ -0,0 +1,13 @@
+package com.jean;
+
+public class Dev {
+
+    private Laptop laptop;
+    int age = 39;
+    public Dev(){
+        System.out.println("Inside the Dev constructor");
+    }
+    public  void build(){
+        System.out.println("working on a dev project");
+    }
+}
---
+++ src/main/java/com/jean/Laptop.java
@@ -0,0 +1,11 @@
+package com.jean;
+
+public class Laptop {
+
+    public Laptop() {
+        System.out.println("Inside the Laptop constructor");
+    }
+    public void compile(){
+        System.out.println("compiling....");
+    }
+}
